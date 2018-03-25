9845f04774dde4ac912a0c76cb6704f7d4dcaa1c
HTarzwell <htarzwell@gmail.com>
2018-03-25 11:42:58 -0400

    My dumb ideas worked!  YAY!
    - Duration count added to both Handlebars scripts
    - Index.js updated with a show/hide function that is keyed to the
    user's selection of the "other" option in a drop-down menu
    - Index.js added functions that push list option values into hidden
    input field for sending to database

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,8 +18,14 @@ $(() => {
   authEvents.addHandlers()
   downTimeEvents.addHandlers()
   // $('#after-signinauth').hide()
-  $('.other-input').hide()
+  $('.value-text').hide()
   $('select').change(function () {
-    $('.other-input').show()
+    $('.value-text').html('<input type="text" class="form-control other-input" name="downtime_instance[activity]" placeholder="Other">')
+    $('.other-input').val(this.value)
+    if (this.value === 'Other (Enter Your Downtime Here)') {
+      $('.value-text').show()
+    } else {
+      $('.value-text').hide()
+    }
   })
 })
